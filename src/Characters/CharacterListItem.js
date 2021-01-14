3b9ae5e9eb4fc294e9011cf937f3bc30dab0d791
froggy <chrisdetmering@gmail.com>
2021-01-13 16:27:19 -0800

    Added body styling and fixed units being shown when characters have unknown mass and height

--- src/Characters/CharacterListItem.js
+++ src/Characters/CharacterListItem.js
@@ -4,12 +4,15 @@ export function CharacterListItem(props) {
   const {name, birth_year, height, mass, homeworld, species}
   = props.character; 
   
+  const Height = height === "unknown" ? height : height + "in";
+  const Mass = mass === "unknown" ? mass : mass + "kg";
+
   return(<>
     <tr>
       <td>{name}</td>
       <td>{birth_year}</td>
-      <td>{height}in.</td>
-      <td>{mass}kg.</td>
+      <td>{Height}</td>
+      <td>{Mass}</td>
       <td>{homeworld}</td>
       <td>{species}</td> 
     </tr>
